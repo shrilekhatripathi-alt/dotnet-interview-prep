cf8c04e4d8bc350ee59a728d51fa05244f1cbd62
Shrilekha <shrilekha.tripathi@gmail.com>
2025-12-21 18:08:24 +0100

    Day 4:Advanced OOPS-Interface,absatract,virtual,sealed,method hiding

---
+++ Day4.ConsoleApp/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Day3.ConsoleApp
+{
+    interface IAnimal
+    {
+        void Speak();
+    }
+    abstract class Animal
+    {
+        public abstract void Eat();
+        public virtual void Sound()
+        {
+            Console.WriteLine("Animal Sound");
+        }
+    }
+    class Dog : Animal, IAnimal
+    {
+        public override void Eat()
+        {
+            Console.WriteLine("Dog Eats");
+        }
+        public override void Sound()
+        {
+            Console.WriteLine("Dog Barks");
+        }
+
+        public void Speak()
+        {
+            Console.WriteLine("Dog Speaks");
+        }
+    }
+    sealed class FinalClass
+    {
+        public void Show()
+        {
+            Console.WriteLine("Sealed class method");
+        }
+    }
+    class Parent
+    {
+        public void Display()
+        {
+            Console.WriteLine("Parent Display");
+        }
+    }
+    class Child : Parent 
+    {
+        public new void Display()
+        {
+            Console.WriteLine("Child Display");
+        }
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Animal a = new Dog();
+            a.Sound();
+            a.Eat();
+
+            Child c = new Child();
+            c.Display();
+        }
+    }
+}
