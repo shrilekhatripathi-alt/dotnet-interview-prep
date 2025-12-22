1800786664a10405d236595353e3a35efa153e5b
Shrilekha <shrilekha.tripathi@gmail.com>
2025-12-22 13:45:47 +0100

    Day5: Memory Management,GC,IDisposable,using,shallow and deep copy

---
+++ Day5.ConsoleApp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Net.NetworkInformation;
+
+namespace Day5.ConsoleApp
+{
+    class Resource : IDisposable
+    {
+        public void Use()
+        {
+            Console.WriteLine("using resource");
+        }
+        public void Dispose()
+        {
+            Console.WriteLine("resource disposed");
+        }
+        ~Resource()
+        {
+            Console.WriteLine("finalize called");
+        }
+    }
+    class Person
+    {
+        public int Age;
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //GC example
+            Resource r = new Resource();
+            r.Use();
+            r = null;
+            GC.Collect();
+
+            // Using example
+            using (Resource res = new Resource())
+            {
+                res.Use();
+            }
+
+            //Shallow copy example
+            Person p1 = new Person { Age = 30 };
+            Person p2 = p1;
+            p2.Age = 40;
+            Console.WriteLine($"shallow copy {p1.Age}");
+
+        }
+
+
+    }
+}
