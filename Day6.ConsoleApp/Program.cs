2c220f8c424b4a3bcad330865a25fbd213f919fb
Shrilekha <shrilekha.tripathi@gmail.com>
2025-12-23 16:53:58 +0100

    Day 6: Exception handling, try,catch,throw,finally and custom exception

---
+++ Day6.ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace Day6.CosoleApp
+{
+    class AgeException : Exception
+    {
+        public AgeException(string message) : base(message)
+        {
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int age = -5;
+                validateAge(age);
+            }
+            catch (AgeException ex)
+            {
+                Console.WriteLine($"Custom exception:{ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"General exception : {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Finally block executed");
+            }
+        }
+
+        static void validateAge(int age)
+        {
+            if (age < 0)
+            {
+                throw new AgeException("age can not be negative");
+            }
+
+        }
+    }
+}
